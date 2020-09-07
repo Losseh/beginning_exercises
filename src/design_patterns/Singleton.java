26a5b17634ba83141d4234b82d338bac66c784b7
Adrian Szymanski (ext.) <Adrian.Szymanski@partner.bmw.de>
2020-09-07 10:26:42 +0200

    design patterns

---
+++ src/design_patterns/Singleton.java
@@ -0,0 +1,12 @@
+package design_patterns;
+
+public class Singleton {
+    private Singleton() {
+
+    }
+
+    // TODO fill
+    public static Singleton getInstance() {
+        return null;
+    }
+}
